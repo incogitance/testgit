757ade0a09098b45e762697f7ef4dad2d99f7e94
Jordan Heskett <jordanheskett@gmail.com>
2019-10-28 02:06:57 -0500

    added mocha chai tests and created HelloWorld app

--- newfile.js
+++ newfile.js
@@ -2,3 +2,16 @@
 // last attempt failed at push trying again....
 
 // push with ubuntu bionic rsa
+//Load express module with `require` directive
+var express = require('express')
+var app = express()
+
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+//Launch listening server on port 8080
+app.listen(8080, function () {
+  console.log('App listening on port 8080!')
+})
